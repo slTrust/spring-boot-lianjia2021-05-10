2bd74bc40d070bd3efaf879f372123342c35c504
slTrust <trustfor@sina.cn>
2020-11-17 22:10:56 +0800

    删除无用代码

--- src/main/java/hello/configuration/JavaConfiguration.java
+++ src/main/java/hello/configuration/JavaConfiguration.java
@@ -1,9 +1,5 @@
 package hello.configuration;
 
-import hello.mapper.UserMapper;
-import hello.service.OrderService;
-import hello.service.UserService;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
