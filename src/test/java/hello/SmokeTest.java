72451c495112ffc04c9c84249dff7bff3f049e91
slTrust <trustfor@sina.cn>
2020-11-18 01:21:22 +0800

    添加测试用库，解决 SmokeTest 一直报错问题 - 乃 spring-boot-starter-parent 版本有误

---
+++ src/test/java/hello/SmokeTest.java
@@ -0,0 +1,12 @@
+package hello;
+
+
+import org.junit.jupiter.api.Test;
+
+public class SmokeTest {
+    @Test
+    public void test(){
+        System.out.println(2);
+//        throw new IllegalArgumentException();
+    }
+}
