5f7389d31477de37718050bab2f59268e6b936a3
slTrust <trustfor@sina.cn>
2020-11-17 17:53:13 +0800

    项目清理

--- src/main/java/hello/configuration/JavaConfiguration.java
+++ src/main/java/hello/configuration/JavaConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JavaConfiguration {
 
+    /*
     @Bean
     public UserService someDummyBean1(UserMapper userMapper) {
         return new UserService(userMapper);
@@ -17,4 +18,13 @@ public class JavaConfiguration {
     public OrderService someDummyBean2(UserService userService) {
         return new OrderService(userService);
     }
+    */
+
+    /*
+    使用 代替这里的 @Bean
+    @Service
+    public class OrderService {
+       ...
+    }
+    */
 }
